355bf5af24b9ae125b811436f273a11c60642231
ChanggunLee <cd8114@gmail.com>
2017-12-12 01:18:02 +0900

    bug fix

--- spaceShooter.py
+++ spaceShooter.py
@@ -17,9 +17,8 @@
 
 from __future__ import division
 import pygame
-
+from os import path
 import random
-import setting
 import main_menu
 import draw
 import explosion
@@ -28,6 +27,40 @@ import missile
 import mob
 import play
 import pow
+
+## assets folder
+img_dir = path.join(path.dirname(__file__), 'assets')
+sound_folder = path.join(path.dirname(__file__), 'sounds')
+
+###############################
+## to be placed in "constant.py" later
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+POWERUP_TIME = 5000
+BAR_LENGTH = 100
+BAR_HEIGHT = 10
+
+# Define Colors 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+###############################
+
+###############################
+## to placed in "__init__.py" later
+## initialize pygame and create window
+pygame.init()
+pygame.mixer.init()  ## For sound
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+clock = pygame.time.Clock()     ## For syncing the FPS
+###############################
+
+font_name = pygame.font.match_font('arial')
 
 
 ###################################################
