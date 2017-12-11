cb920b5dc1d260e4ca989877d857d33208ae3da1
ChanggunLee <cd8114@gmail.com>
2017-12-12 03:54:49 +0900

    add pow

--- spaceshooter/spaceShooter.py
+++ spaceshooter/spaceShooter.py
@@ -23,32 +23,14 @@ from menu import *
 from draw import *
 from explosion import *
 from player import *
+from mob import *
+from pow import *
+
 
 def newmob():
     mob_element = Mob()
     all_sprites.add(mob_element)
     mobs.add(mob_element)
-
-## defines the sprite for Powerups
-class Pow(pygame.sprite.Sprite):
-    def __init__(self, center):
-        pygame.sprite.Sprite.__init__(self)
-        self.type = random.choice(['shield', 'gun'])
-        self.image = powerup_images[self.type]
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        ## place the bullet according to the current position of the player
-        self.rect.center = center
-        self.speedy = 2
-
-    def update(self):
-        """should spawn right in front of the player"""
-        self.rect.y += self.speedy
-        ## kill the sprite after it moves over the top border
-        if self.rect.top > HEIGHT:
-            self.kill()
-
-            
 
 ## defines the sprite for bullets
 class Bullet(pygame.sprite.Sprite):
