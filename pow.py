ce6b566bdb5a5b073eaf90663b1f6451753a1130
ChanggunLee <cd8114@gmail.com>
2017-12-12 01:11:33 +0900

    division

---
+++ pow.py
@@ -0,0 +1,20 @@
+
+## defines the sprite for Powerups
+class Pow(pygame.sprite.Sprite):
+    def __init__(self, center):
+        pygame.sprite.Sprite.__init__(self)
+        self.type = random.choice(['shield', 'gun'])
+        self.image = powerup_images[self.type]
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        ## place the bullet according to the current position of the player
+        self.rect.center = center
+        self.speedy = 2
+
+    def update(self):
+        """should spawn right in front of the player"""
+        self.rect.y += self.speedy
+        ## kill the sprite after it moves over the top border
+        if self.rect.top > HEIGHT:
+            self.kill()
+
