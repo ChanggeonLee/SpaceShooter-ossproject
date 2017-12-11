9e4bf96f3e09e73e3da15dde416907bf47f452e2
ChanggunLee <cd8114@gmail.com>
2017-12-12 04:04:15 +0900

    add load_img amd load_sound

---
+++ spaceshooter/load_sound.py
@@ -0,0 +1,18 @@
+import pygame
+from os import path
+from setting import *
+
+
+###################################################
+### Load all game sounds
+shooting_sound = pygame.mixer.Sound(path.join(sound_folder, 'pew.wav'))
+missile_sound = pygame.mixer.Sound(path.join(sound_folder, 'rocket.ogg'))
+expl_sounds = []
+for sound in ['expl3.wav', 'expl6.wav']:
+    expl_sounds.append(pygame.mixer.Sound(path.join(sound_folder, sound)))
+## main background music
+#pygame.mixer.music.load(path.join(sound_folder, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.2)      ## simmered the sound down a little
+
+player_die_sound = pygame.mixer.Sound(path.join(sound_folder, 'rumble1.ogg'))
+###################################################
--- spaceshooter/spaceShooter.py
+++ spaceshooter/spaceShooter.py
@@ -25,7 +25,8 @@ from explosion import *
 from player import *
 from mob import *
 from pow import *
-
+from load_img import *
+from load_sound import * 
 
 def newmob():
     mob_element = Mob()
@@ -73,76 +74,6 @@ class Missile(pygame.sprite.Sprite):
             self.kill()
 
 
-###################################################
-## Load all game images
-
-background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
-background_rect = background.get_rect()
-## ^^ draw this rect first 
-
-player_img = pygame.image.load(path.join(img_dir, 'playerShip1_orange.png')).convert()
-player_mini_img = pygame.transform.scale(player_img, (25, 19))
-player_mini_img.set_colorkey(BLACK)
-bullet_img = pygame.image.load(path.join(img_dir, 'laserRed16.png')).convert()
-missile_img = pygame.image.load(path.join(img_dir, 'missile.png')).convert_alpha()
-# meteor_img = pygame.image.load(path.join(img_dir, 'meteorBrown_med1.png')).convert()
-meteor_images = []
-meteor_list = [
-    'meteorBrown_big1.png',
-    'meteorBrown_big2.png', 
-    'meteorBrown_med1.png', 
-    'meteorBrown_med3.png',
-    'meteorBrown_small1.png',
-    'meteorBrown_small2.png',
-    'meteorBrown_tiny1.png'
-]
-
-for image in meteor_list:
-    meteor_images.append(pygame.image.load(path.join(img_dir, image)).convert())
-
-## meteor explosion
-explosion_anim = {}
-explosion_anim['lg'] = []
-explosion_anim['sm'] = []
-explosion_anim['player'] = []
-for i in range(9):
-    filename = 'regularExplosion0{}.png'.format(i)
-    img = pygame.image.load(path.join(img_dir, filename)).convert()
-    img.set_colorkey(BLACK)
-    ## resize the explosion
-    img_lg = pygame.transform.scale(img, (75, 75))
-    explosion_anim['lg'].append(img_lg)
-    img_sm = pygame.transform.scale(img, (32, 32))
-    explosion_anim['sm'].append(img_sm)
-
-    ## player explosion
-    filename = 'sonicExplosion0{}.png'.format(i)
-    img = pygame.image.load(path.join(img_dir, filename)).convert()
-    img.set_colorkey(BLACK)
-    explosion_anim['player'].append(img)
-
-## load power ups
-powerup_images = {}
-powerup_images['shield'] = pygame.image.load(path.join(img_dir, 'shield_gold.png')).convert()
-powerup_images['gun'] = pygame.image.load(path.join(img_dir, 'bolt_gold.png')).convert()
-
-
-###################################################
-
-
-###################################################
-### Load all game sounds
-shooting_sound = pygame.mixer.Sound(path.join(sound_folder, 'pew.wav'))
-missile_sound = pygame.mixer.Sound(path.join(sound_folder, 'rocket.ogg'))
-expl_sounds = []
-for sound in ['expl3.wav', 'expl6.wav']:
-    expl_sounds.append(pygame.mixer.Sound(path.join(sound_folder, sound)))
-## main background music
-#pygame.mixer.music.load(path.join(sound_folder, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
-pygame.mixer.music.set_volume(0.2)      ## simmered the sound down a little
-
-player_die_sound = pygame.mixer.Sound(path.join(sound_folder, 'rumble1.ogg'))
-###################################################
 
 ## TODO: make the game music loop over again and again. play(loops=-1) is not working
 # Error : 
