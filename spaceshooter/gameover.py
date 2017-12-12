53579921fe3a857d22d5cadeeb588efe1090cfa6
ChanggunLee <cd8114@gmail.com>
2017-12-12 13:57:00 +0900

    add game over

---
+++ spaceshooter/gameover.py
@@ -0,0 +1,40 @@
+import pygame
+from os import path
+from draw import *
+from setting import *
+    
+def main_menu():
+    global screen
+
+    menu_song = pygame.mixer.music.load(path.join(sound_folder, "menu.ogg"))
+    pygame.mixer.music.play(-1)
+
+    title = pygame.image.load(path.join(img_dir, "main.png")).convert()
+    title = pygame.transform.scale(title, (WIDTH, HEIGHT), screen)
+
+    screen.blit(title, (0,0))
+    pygame.display.update()
+
+    while True:
+        ev = pygame.event.poll()
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_RETURN:
+                break
+            elif ev.key == pygame.K_q:
+                pygame.quit()
+                quit()
+        elif ev.type == pygame.QUIT:
+                pygame.quit()
+                quit() 
+        else:
+            draw_text(screen, "Game Over", 30, WIDTH/2, HEIGHT/2)
+            draw_text(screen, "Press [ENTER] To Begin", 30, WIDTH/2, (HEIGHT/2)+40)
+            draw_text(screen, "or [Q] To Quit", 30, WIDTH/2, (HEIGHT/2)+40)
+            pygame.display.update()
+
+    #pygame.mixer.music.stop()
+    ready = pygame.mixer.Sound(path.join(sound_folder,'getready.ogg'))
+    ready.play()
+    screen.fill(BLACK)
+    draw_text(screen, "GET READY!", 40, WIDTH/2, HEIGHT/2)
+    pygame.display.update()
